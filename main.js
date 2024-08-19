bf44e4973f61ad69aa040a9f6edc14d34b26ac1b
Usheninte Dangana <ninte@nullnow.com>
2024-08-19 13:57:56 +0100

    Completed add and remove grades section

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
-// Initialize an Array of Grades
+// Initialize an array of grades.
 
 const primaryGrades = [92, 85, 78, 95, 88, 72, 65, 90, 83, 75];
+
+// Use push to add a grade to the end of the array.
+
+primaryGrades.push(73);
+
+// Use pop to remove the last grade from the array.
+
+primaryGrades.pop();
+
+// Use unshift to add a grade to the beginning of the array.
+
+primaryGrades.unshift(24);
+
+// Use shift to remove the first grade from the array.
+
+primaryGrades.shift();
